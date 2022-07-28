ae4ad114af2b4237012c68b9c25609ace521f143
wassim laouini <wassimlaouini02@gmail.com>
2022-07-29 01:16:45 +0200

    ...

---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app-bar app color="white" flat>
+    <v-badge color="#D5F0DB" dot>
+      
+      <v-img src="1.png" contain width="30"></v-img>
+    </v-badge>
+
+    <v-toolbar-title class="ml-4">
+      <span class="green--text">e</span><strong>Restau name </strong>
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+    <v-badge color="#FF6D59" overlap content="2" class="mr-2 mt-1">
+      <v-avatar color="#FFF0EE" size="40">
+        <v-icon x-small color="#FF6D59">far fa-heart</v-icon>
+      </v-avatar>
+    </v-badge>
+    <v-badge color="#41AB55" overlap content="3" class="mr-2 mt-1">
+      <v-avatar color="#ECF7EE" size="40">
+        <v-icon x-small color="#41AB55">fas fa-shopping-cart</v-icon>
+      </v-avatar>
+    </v-badge>
+   
+   
+   
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.v-toolbar__title {
+  font-size: 1rem !important;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,156 @@
+<template>
+  <v-app>
+    <Navbar />
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12">
+          <v-card class="mx-auto rounded-xl" flat>
+            <v-img src="cover2.png" max-height="100%" max-width="100%" contain>
+              <v-card-title class="top ml-3">
+                <h2 class="title1">Order your</h2>
+              </v-card-title>
+              <v-card-title class="ml-3 mt-n8">
+                <h2 class="title2">Your favourite food without weaigter  </h2>
+              </v-card-title>
+              <v-card-text class="ml-3">
+                <h3 class="green--text">#Table Delivery</h3>
+              </v-card-text>
+              <v-card-text class="ml-10">
+                <v-row>
+                  <v-col cols="4">
+                    <v-text-field
+                      label="Search your daily groceries"
+                      class="d-none d-sm-none d-md-flex"
+                      filled
+                      prepend-inner-icon="mdi-magnify"
+                      solo
+                      flat
+                      background-color=""
+                      rounded
+                      outlined
+                      success
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="2" class="marginLeft">
+                    <v-btn
+                      color="green"
+                      rounded
+                      dark
+                      x-large
+                      class="py-7 d-none d-sm-none d-md-flex"
+                    >
+                      search
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-img>
+          </v-card>
+          <v-toolbar flat color="transparent">
+            <v-toolbar-title class="text-h6">Category</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn-toggle v-model="toggle_exclusive" group color="#49D9A0">
+              <v-btn>
+                <v-icon>mdi-chevron-left</v-icon>
+              </v-btn>
+              <v-btn>
+                <v-icon>mdi-chevron-right</v-icon>
+              </v-btn>
+            </v-btn-toggle>
+          </v-toolbar>
+
+          <Category />
+
+          <v-toolbar flat color="transparent" class="mt-5">
+            <v-toolbar-title class="text-h6">Popular Product</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn rounded color="green" dark class="px-8">See All</v-btn>
+          </v-toolbar>
+          <Product />
+          <v-card flat color="#E2F2E5" class="rounded-xl mt-8">
+            <v-toolbar flat color="transparent" class="mb-4">
+              <v-toolbar-title class="text-h6 mt-5"
+                >Popular Bundle Pack</v-toolbar-title
+              >
+              <v-spacer></v-spacer>
+            </v-toolbar>
+            <Pack />
+          </v-card>
+         
+          
+  
+        </v-col>
+      </v-row>
+    </v-container>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Category from "../components/Category.vue";
+import Product from "../components/Product.vue";
+import Pack from "../components/Pack.vue";
+
+
+export default {
+  name: "Home",
+  data: () => ({
+    toggle_exclusive: 1,
+  }),
+  components: {
+    Navbar,
+    Footer,
+    Category,
+    Product,
+    Pack,
+    
+    
+  },
+};
+</script>
+<style >
+.marginLeft {
+  margin-left: -90px;
+}
+.mtop {
+  margin-top: 100px;
+}
+.mbottom {
+  margin-bottom: 100px;
+}
+.v-card.borderme {
+  border: 2px solid green !important;
+}
+.v-card.borderout {
+  border: 1px solid #d5f0db !important;
+}
+.v-btn:not(.v-btn--round).v-size--default.add {
+  min-width: 0px !important;
+}
+.theme--light.v-sheet--outlined.mobile {
+  border: 2px solid black !important;
+}
+@media only screen and (max-width: 600px) {
+  h2.title1 {
+    font-size: 15px;
+  }
+  h2.title2 {
+    font-size: 15px;
+  }
+  .top {
+    margin-top: 20px;
+  }
+}
+@media only screen and (min-width: 600px) {
+  .top {
+    margin-top: 70px;
+  }
+}
+@media only screen and (min-width: 768px) {
+  .top {
+    margin-top: 120px;
+  }
+}
+</style>
